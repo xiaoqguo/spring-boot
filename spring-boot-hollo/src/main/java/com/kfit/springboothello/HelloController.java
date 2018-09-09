ddb6b05b36f7d38c1909bba3cc6ddfe102f0bdc8
guo zheng mao <xiaoqguo@126.com>
2018-09-09 22:21:18 +0800

    1、写一个Controller 2、在Controller中 引入 @RestController 3、写一个@RequestMapping("/hello") 方法 4、启动程序，Hello Word就写好了！！

---
+++ spring-boot-hollo/src/main/java/com/kfit/springboothello/HelloController.java
@@ -0,0 +1,14 @@
+package com.kfit.springboothello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//@RestController 等价于 @Controller+@RequestBody
+@RestController
+public class HelloController {
+
+    //使用RequestMapping 建立请求映射
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
--- spring-boot-hollo/src/main/java/com/kfit/springboothello/SpringBootHelloApplication.java
+++ spring-boot-hollo/src/main/java/com/kfit/springboothello/SpringBootHelloApplication.java
@@ -2,11 +2,12 @@ package com.kfit.springboothello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//SpringBootApplication 指定这是一个Spring 程序
 @SpringBootApplication
 public class SpringBootHelloApplication {
 
     public static void main(String[] args) {
+        //启动应用程序
         SpringApplication.run(SpringBootHelloApplication.class, args);
     }
 }
